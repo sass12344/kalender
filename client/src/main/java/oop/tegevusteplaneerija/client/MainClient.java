22ee053dc7f51ea7c1abb437055f8b9c9cfe1101
MisTeha <uku.mihkel.kolka@gmail.com>
2025-03-28 00:06:30 +0200

    fix minor problems

--- client/src/main/java/oop/tegevusteplaneerija/client/MainClient.java
+++ client/src/main/java/oop/tegevusteplaneerija/client/MainClient.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 
 public class MainClient extends Application {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         launch(args);
     }
 
